d89315128f3007d6668133f0bc620d0393867e15
Thisara Rathnapala <thisaratr94@gmail.com>
2023-08-12 13:01:27 +0530

    Prime Numbers

--- PrintingAlphabet.java
+++ PrintingAlphabet.java
@@ -1,6 +1,5 @@
 public class PrintingAlphabet {
     public static void main(String[] args) {
-        int num = 65;
         char letter = 0;
         for (int i = 0; i < 26; i++) {
             letter = (char) (65+i);
